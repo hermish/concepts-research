74bcc4a37caeaf312c766e0745656cd68eda81a5
Hermish Mehta <hermishdm@gmail.com>
2018-01-14 16:34:11 -0800

    Complete analysis; set up additional for further analysis

--- analysis/cogsci-paper/utilities/describe.py
+++ analysis/cogsci-paper/utilities/describe.py
@@ -1,16 +1,19 @@
-import numpy as np
 import scipy.stats as stats
 
 
-def independent_t_test(first, second, label):
+def independent_t_test(first, second):
     t, prob = stats.ttest_ind(first, second)
     winner = 0 if first.mean() > second.mean() else 1
-    return '{}: t = {:.3}, p = {:.3} ({} greater)'.format(
-        label, t, prob, winner)
+    return 't = {:.3}, p = {:.3} ({} greater)'.format(t, prob, winner)
 
 
-def paired_t_test(first, second, label):
-    t, prob = stats.stats.ttest_rel(first, second)
+def paired_t_test(first, second):
+    t, prob = stats.ttest_rel(first, second)
     winner = 0 if first.mean() > second.mean() else 1
-    return '{}: t = {:.3}, p = {:.3} ({} greater)'.format(
-        label, t, prob, winner)
+    return 't = {:.3}, p = {:.3} ({} greater)'.format(t, prob, winner)
+
+
+def one_sample_t_test(sample, expected_mean):
+    t, prob = stats.ttest_1samp(sample, expected_mean)
+    relationship = 'smaller' if sample.mean() < expected_mean else 'greater'
+    return 't = {:.3}, p = {:.3} ({})'.format(t, prob, relationship)
