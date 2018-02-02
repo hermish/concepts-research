aa9837f55a05547e34f4e4017d25459e53e596e0
Hermish Mehta <hermishdm@gmail.com>
2018-02-01 21:59:17 -0800

    Finalize figures and analysis

--- analysis/5-cogsci-paper/figures/data.R
+++ analysis/5-cogsci-paper/figures/data.R
@@ -1,8 +1,8 @@
 # EXPERIMENT 1
 # Graph 1
 
-exp_one_judgments <- data.frame(
-	votes=c("high", "high", "low", "low"),
+graph <- data.frame(
+	votes=factor(c("high", "high", "low", "low"), levels=c("low", "high")),
 	judgment=c("popularity", "curiosity", "popularity", "curiosity"),
 	mean=c(4.533999999999997,
 		3.206666666666666,
@@ -15,7 +15,7 @@ exp_one_judgments <- data.frame(
 )
 
 ggplot(
-	data=exp_one_judgments,
+	data=graph,
 	aes(x=judgment, y=mean, fill=votes)
 ) +
 geom_bar(stat="identity", position = "dodge") + 
@@ -31,17 +31,17 @@ scale_fill_manual(values=c("#D9B8C5", "#88ABC2"))
 
 # Graph 2
 
-exp_one_proportion <- data.frame(
+graph <- data.frame(
 	label=factor(c("Low Scores", "High Scores"), levels=c("Low Scores", "High Scores")),
 	proportion=c(0.3527397260273976, 0.6472602739726027),
 	se=c(0.013209931327771474, 0.013209931327771474)
 )
 
 ggplot(
-	data=exp_one_proportion,
+	data=graph,
 	aes(x=label, y=proportion)
 ) +
-geom_bar(stat="identity", position = "dodge", width = 0.5, fill="#B1D7BF") + 
+geom_bar(stat="identity", position = "dodge", width = 0.75, fill="#B1D7BF") + 
 geom_errorbar(
 	aes(
 		ymin=proportion-se, ymax=proportion+se),
@@ -54,7 +54,7 @@ theme_minimal()
 # EXPERIMENT 2
 # Graph 1
 
-exp_two_judgments <- data.frame(
+graph <- data.frame(
 	votes=factor(
 		c(
 			"high", "high", "high", "high", "high", "high", "high",
@@ -62,12 +62,12 @@ exp_two_judgments <- data.frame(
 		),
 		levels=c("low", "high")
 	),
-	judgment=c(
+	judgment=factor(c(
 		'Curiosity', 'Confidence', 'Usefulness',
 		'Popularity', 'Writing', 'Surprise', 'Social Utility',
 		'Curiosity', 'Confidence', 'Usefulness',
 		'Popularity', 'Writing', 'Surprise', 'Social Utility'
-	),
+	), levels=c('Curiosity', 'Popularity', 'Confidence', 'Surprise', 'Social Utility', 'Usefulness', 'Writing')),
 	mean=c(
 		3.3986301369863003, 1.7575342465753403, 2.6363013698630144, 4.075342465753427, 3.9417808219178068, 2.8068493150684954, 2.3979452054794512,
 		2.9431506849315054, 1.7636986301369875, 2.3458904109589063, 1.8020547945205476, 3.685616438356163, 2.2780821917808214, 2.0445205479452033
@@ -79,7 +79,7 @@ exp_two_judgments <- data.frame(
 )
 
 ggplot(
-	data=exp_two_judgments,
+	data=graph,
 	aes(x=judgment, y=mean, fill=votes)
 ) +
 geom_bar(stat="identity", position = "dodge") + 
@@ -95,7 +95,7 @@ scale_fill_manual(values=c("#D9B8C5", "#88ABC2"))
 
 # Graph 1'
 
-exp_two_judgments <- data.frame(
+graph <- data.frame(
 	votes=factor(
 		c(
 			"high", "high", "high", "high", "high", "high",
@@ -103,12 +103,12 @@ exp_two_judgments <- data.frame(
 		),
 		levels=c("low", "high")
 	),
-	judgment=c(
+	judgment=factor(c(
 		'Curiosity', 'Confidence', 'Usefulness',
 		'Popularity', 'Surprise', 'Social Utility',
 		'Curiosity', 'Confidence', 'Usefulness',
 		'Popularity', 'Surprise', 'Social Utility'
-	),
+	), levels=c('Curiosity', 'Popularity', 'Confidence', 'Surprise', 'Social Utility', 'Usefulness')),
 	mean=c(
 		3.3986301369863003, 1.7575342465753403, 2.6363013698630144, 4.075342465753427, 2.8068493150684954, 2.3979452054794512,
 		2.9431506849315054, 1.7636986301369875, 2.3458904109589063, 1.8020547945205476, 2.2780821917808214, 2.0445205479452033
@@ -120,7 +120,7 @@ exp_two_judgments <- data.frame(
 )
 
 ggplot(
-	data=exp_two_judgments,
+	data=graph,
 	aes(x=judgment, y=mean, fill=votes)
 ) +
 geom_bar(stat="identity", position = "dodge") + 
@@ -136,17 +136,17 @@ scale_fill_manual(values=c("#D9B8C5", "#88ABC2"))
 
 # Graph 2
 
-exp_two_proportion <- data.frame(
+graph <- data.frame(
 	label=factor(c("Low Scores", "High Scores"), levels=c("Low Scores", "High Scores")),
 	proportion=c(0.4520547945205479, 0.5479452054794521),
 	se=c(0.01132013052992385, 0.011320130529923852)
 )
 
 ggplot(
-	data=exp_two_proportion,
+	data=graph,
 	aes(x=label, y=proportion)
 ) +
-geom_bar(stat="identity", position = "dodge", width = 0.5, fill="#B1D7BF") + 
+geom_bar(stat="identity", position = "dodge", width = 0.75, fill="#B1D7BF") + 
 geom_errorbar(
 	aes(
 		ymin=proportion-se, ymax=proportion+se),
